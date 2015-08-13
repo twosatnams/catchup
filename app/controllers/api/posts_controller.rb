3b2463eb219e133698d8d3cfeb4a9e903180ba1b
Satnam <satnamxv63@gmail.com>
2015-08-13 11:02:49 -0700

    add post model associations

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,14 @@
 class Api::PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+
+  has_many :photos,
+    class_name: "Photo",
+    foreign_key: :post_id,
+    primary_key: :id
+
+  has_many :likes,
+    class_name: "Like",
+    foreign_key: :post_id,
+    primary_key: :id
+
+  has_many :mentions,
+    class_name: "Mention",
+    foreign_key: :post_id,
+    primary_key: :id
+
+  has_many :comments,
+    class_name: "Comment",
+    foreign_key: :post_id,
+    primary_key: :id
+
 end
