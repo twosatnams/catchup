387a9c2e5ea432e918119941eac8bba96a08cef0
Satnam <satnamxv63@gmail.com>
2015-08-13 18:33:42 -0700

    update routes for likes, mentions, photos and comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { :format => :json } do
     resources :users, only: [:index, :show, :update]
     resources :posts, only: [:create, :update, :destroy, :show]
+    resources :likes, only: [:create, :destroy]
+    resources :photos, only: [:create, :destroy, :show]
+    resources :comments, only: [:create, :update, :show, :destroy]
+    resources :mentions, only: [:create, :destroy]
   end
 end
