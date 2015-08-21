da54ce6e8898df724cc00f1c273761e8fb607a60
Satnam <satnamxv63@gmail.com>
2015-08-21 09:36:21 -0700

    add dynamic templates for images

--- app/assets/javascripts/views/posts/photo/photo_show.js
+++ app/assets/javascripts/views/posts/photo/photo_show.js
@@ -1,26 +1,21 @@
 Catchup.Views.PhotoShow = Backbone.View.extend({
-  // template: JST['post/photo/single_photo'],
-  // template: JST['post/photo/double_photo'],
-  template: JST['post/photo/triple_photo'],
 
   initialize: function () {
     this.listenTo(this.collection, 'sync', this.render);
   },
 
-  // template: function () {
-  //   if (this.collection.length === 1) {
-  //     return JST['post/photo/single_photo']();
-  //   } else if (this.collection.length === 2) {
-  //     return JST['post/photo/double_photo'];
-  //   } else {
-  //     return JST['post/photo/triple_photo'];
-  //   }
-  // },
+  template: function (options) {
+    if (this.collection.length === 1) {
+      return JST['post/photo/single_photo'](options);
+    } else if (this.collection.length === 2) {
+      return JST['post/photo/double_photo'](options);
+    } else {
+      return JST['post/photo/triple_photo'](options);
+    }
+  },
 
   render: function () {
-    var content = this.template({
-      photos: this.collection
-    });
+    var content = this.template({ photos: this.collection });
     this.$el.html(content);
     return this;
   }
