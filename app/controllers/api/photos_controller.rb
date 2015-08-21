04ea98dd7e1dbcc81d9a254c858c98abac9a58b1
Satnam <satnamxv63@gmail.com>
2015-08-20 17:31:17 -0700

    add photos controller

---
+++ app/controllers/api/photos_controller.rb
@@ -0,0 +1,25 @@
+class Api::PhotosController < ApplicationController
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      render json: @photo
+    else
+      render json: @photo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    render json: @photo, status: 200
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).require(:url, :post_id)
+  end
+end
