3e385a68895f05002ed6d12a13b4deae6cb2d40c
Satnam <satnamxv63@gmail.com>
2015-08-23 14:10:25 -0700

    basic info decorated

--- app/assets/javascripts/views/profile/basic_info.js
+++ app/assets/javascripts/views/profile/basic_info.js
@@ -1,6 +1,7 @@
 Catchup.Views.BasicInfo = Backbone.View.extend({
   template: JST['profile/basic_info'],
   tagName: 'ul',
+  className: 'list-group basic-info-list',
 
   render: function () {
     var content = this.template({
