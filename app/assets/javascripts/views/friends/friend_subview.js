bb630a841270249c96c22ac5208dc5727888ce3d
Satnam <satnamxv63@gmail.com>
2015-08-23 22:35:58 -0700

    working on friends page

--- app/assets/javascripts/views/friends/friend_subview.js
+++ app/assets/javascripts/views/friends/friend_subview.js
@@ -2,6 +2,7 @@ Catchup.Views.FriendSubview = Backbone.View.extend({
   template: JST['friend/friend_subview'],
 
   tagName: 'li',
+  className: 'friend-list-item',
 
   render: function () {
     var content = this.template({
